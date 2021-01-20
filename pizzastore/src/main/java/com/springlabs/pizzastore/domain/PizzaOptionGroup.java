1306f6caa33302aa834a5277b482525704ff7060
Shan <shanmugarmd@gmail.com>
2021-01-20 13:03:10 +0530

    JoinColumn mapping added for missed entities

--- pizzastore/src/main/java/com/springlabs/pizzastore/domain/PizzaOptionGroup.java
+++ pizzastore/src/main/java/com/springlabs/pizzastore/domain/PizzaOptionGroup.java
@@ -11,11 +11,13 @@ public class PizzaOptionGroup {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "pizza_id")
-    private String pizzaId;
-
-    @Column(name = "option_group_id")
-    private String optionGroupId;
+    @ManyToOne
+    @JoinColumn(name = "pizza_id")
+	private Pizza pizza;
+    
+    @ManyToOne
+    @JoinColumn(name = "option_group_id")
+	private OptionGroup optionGroup;
 
     public Long getId() {
         return id;
@@ -25,28 +27,12 @@ public class PizzaOptionGroup {
         this.id = id;
     }
 
-    public String getPizzaId() {
-        return pizzaId;
-    }
-
-    public void setPizzaId(String pizzaId) {
-        this.pizzaId = pizzaId;
-    }
-
-    public String getOptionGroupId() {
-        return optionGroupId;
-    }
-
-    public void setOptionGroupId(String optionGroupId) {
-        this.optionGroupId = optionGroupId;
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("PizzaOptionGroup{");
         sb.append("id=").append(id);
-        sb.append(", pizzaId='").append(pizzaId).append('\'');
-        sb.append(", optionGroupId='").append(optionGroupId).append('\'');
+        sb.append(", pizzaId='").append(pizza).append('\'');
+        sb.append(", optionGroupId='").append(optionGroup).append('\'');
         sb.append('}');
         return sb.toString();
     }
